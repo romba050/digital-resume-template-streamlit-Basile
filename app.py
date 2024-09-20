8aa6d883137312df39aba17a63f475a4d4835660
Basile Rommes <basilerommes@hotmail.com>
2024-09-20 22:44:40 +0200

    initial commit

--- app.py
+++ app.py
@@ -7,31 +7,27 @@ from PIL import Image
 # --- PATH SETTINGS ---
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
-resume_file = current_dir / "assets" / "CV.pdf"
+resume_file = current_dir / "assets" / "CV_2024-09-20.pdf"
 profile_pic = current_dir / "assets" / "profile-pic.png"
 
 
 # --- GENERAL SETTINGS ---
-PAGE_TITLE = "Digital CV | John Doe"
+PAGE_TITLE = "Digital CV | Basile Rommes"
 PAGE_ICON = ":wave:"
-NAME = "John Doe"
-DESCRIPTION = """
-Senior Data Analyst, assisting enterprises by supporting data-driven decision-making.
-"""
-EMAIL = "johndoe@email.com"
+NAME = "Basile Rommes"
+DESCRIPTION = "Data Scientist, MSc. Bioinformatics"
+EMAIL = "basilerommes@hotmail.com"
 SOCIAL_MEDIA = {
-    "YouTube": "https://youtube.com/c/codingisfun",
-    "LinkedIn": "https://linkedin.com",
-    "GitHub": "https://github.com",
-    "Twitter": "https://twitter.com",
+    "LinkedIn": "https://www.linkedin.com/in/basile-rommes/",
+    "GitHub": "https://github.com/romba050",
 }
 PROJECTS = {
-    "🏆 Sales Dashboard - Comparing sales across three stores": "https://youtu.be/Sb0A9i6d320",
-    "🏆 Income and Expense Tracker - Web app with NoSQL database": "https://youtu.be/3egaMfE9388",
-    "🏆 Desktop Application - Excel2CSV converter with user settings & menubar": "https://youtu.be/LzCfNanQ_9c",
-    "🏆 MyToolBelt - Custom MS Excel add-in to combine Python & Excel": "https://pythonandvba.com/mytoolbelt/",
+    "👁️ : Blood Vessel Segmentation Master Thesis - Leveraging DNN and Bayesian Modeling to find blood vessels": "https://github.com/romba050/MFN_RBV_segmentation",
+    "🧬 Protein Superpositioning - Using Bayesian Inference to position protein strctures over each other": "https://github.com/romba050/Protein_Superpositioning_using_Bayesian_Inference",
+    "🩻 Computer Tomography - Jupyter Notebook on how to use Fourrier Transform to calculate a CT": "https://nbviewer.org/github/romba050/computer_tomography/blob/master/ex3.ipynb",
+    "🎵 Spotle Assist Project - The smart assistant to the Spotle artist guessing game": "https://spotle.streamlit.app/",
+    "📊 NEAR Data Request - Plan your data application to conduct Aging Research": "https://near-data-request.streamlit.app/",
 }
-
 
 st.set_page_config(page_title=PAGE_TITLE, page_icon=PAGE_ICON)
 
@@ -65,19 +61,26 @@ with col2:
 st.write('\n')
 cols = st.columns(len(SOCIAL_MEDIA))
 for index, (platform, link) in enumerate(SOCIAL_MEDIA.items()):
-    cols[index].write(f"[{platform}]({link})")
+    cols[index].link_button(f"{platform}", url=f"{link}")
+
+# --- About ---
+st.write("""
+Data Scientist with Master's Degree in Bioinformatics and focus on machine learning, computer vision, structural bioinformatics and data visualization. 4 years of professional experience in Data Management and Data Harmonisation. Wide area of expertise including data analysis, WebDev and task automation.
+"""
+)
 
 
 # --- EXPERIENCE & QUALIFICATIONS ---
 st.write('\n')
-st.subheader("Experience & Qulifications")
+st.subheader("Experience & Qualifications")
 st.write(
-    """
-- ✔️ 7 Years expereince extracting actionable insights from data
-- ✔️ Strong hands on experience and knowledge in Python and Excel
-- ✔️ Good understanding of statistical principles and their respective applications
-- ✔️ Excellent team-player and displaying strong sense of initiative on tasks
 """
+- ✔️ 4 years experience in clinical data management
+- ✔️ Strong hands on experience and knowledge in Python and R
+- ✔️ Focus on Bioinformatics in Protein Structures and Medicla Image Analysis 
+- ✔️ Good understanding of statistical principles and Machine Learning and their respective applications
+- ✔️ Experience in WebDev using PHP, Wordpress or Python Libraries
+""" #- ✔️ Excellent team-player and displaying strong sense of initiative on tasks
 )
 
 
@@ -86,10 +89,10 @@ st.write('\n')
 st.subheader("Hard Skills")
 st.write(
     """
-- 👩‍💻 Programming: Python (Scikit-learn, Pandas), SQL, VBA
-- 📊 Data Visulization: PowerBi, MS Excel, Plotly
-- 📚 Modeling: Logistic regression, linear regression, decition trees
-- 🗄️ Databases: Postgres, MongoDB, MySQL
+- 👩‍💻 Programming: Python (Scikit-learn, Pandas), R, Unix/Bash, C++, SQL, PHP
+- 📊 Data Visulization: Matplotlib, Plotly, Streamlit
+- 📚 Modeling: Convolutional Neural Networks, Bayesian Maximum a posteriori estimation, Logistic regression, Linear regression, Decision trees
+- 🗄️ Databases and Cloud: MySQL, MariaDB, AWS
 """
 )
 
@@ -100,40 +103,29 @@ st.subheader("Work History")
 st.write("---")
 
 # --- JOB 1
-st.write("🚧", "**Senior Data Analyst | Ross Industries**")
-st.write("02/2020 - Present")
+st.write("🚧", "**Software Developer | Karolinska Institutet**")
+st.write("09/2022 - 09/2024")
 st.write(
     """
-- ► Used PowerBI and SQL to redeﬁne and track KPIs surrounding marketing initiatives, and supplied recommendations to boost landing page conversion rate by 38%
-- ► Led a team of 4 analysts to brainstorm potential marketing and sales improvements, and implemented A/B tests to generate 15% more client leads
-- ► Redesigned data model through iterations that improved predictions by 12%
+- ► Data harmonization for Epidemiological Research
+- ► Maintenance of the [NEAR database](https://neardb.near-aging.se/) and Website
+- ► Building pipelines in R and python,
+- ► WebDev in PHP and Wordpress
 """
 )
 
 # --- JOB 2
 st.write('\n')
-st.write("🚧", "**Data Analyst | Liberty Mutual Insurance**")
-st.write("01/2018 - 02/2022")
+st.write("🚧", "**Research and Development Specialist| Luxembourg Centre for Systems Biomedicine**")
+st.write("09/2020 - 08/2022")
 st.write(
     """
-- ► Built data models and maps to generate meaningful insights from customer data, boosting successful sales eﬀorts by 12%
-- ► Modeled targets likely to renew, and presented analysis to leadership, which led to a YoY revenue increase of $300K
-- ► Compiled, studied, and inferred large amounts of data, modeling information to drive auto policy pricing
+- ► Data management and curation within the european BIOMAP and the luxembourgish CON-VINCE projects
+- ► Using RedCAP to capture medical data
+- ► Building re-usable data pipelines in KNIME
+- ► Teaching Master of Data Science Course in Python and R Programming
 """
 )
-
-# --- JOB 3
-st.write('\n')
-st.write("🚧", "**Data Analyst | Chegg**")
-st.write("04/2015 - 01/2018")
-st.write(
-    """
-- ► Devised KPIs using SQL across company website in collaboration with cross-functional teams to achieve a 120% jump in organic traﬃc
-- ► Analyzed, documented, and reported user survey results to improve customer communication processes by 18%
-- ► Collaborated with analyst team to oversee end-to-end process surrounding customers' return data
-"""
-)
-
 
 # --- Projects & Accomplishments ---
 st.write('\n')
